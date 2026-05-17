9fdb35baca21cb77b0adea80644fa7d33d5bc9ef
noemaier4-gif <noemaier4@gmail.com>
2026-05-17 13:11:26 +0300

    Add student properties and toggle functionality
    Added properties for Student1, Student2, and CurrentStudent. Initialized two student instances in the constructor and set up a method to toggle between them.

--- StudentBinding/MainPage.xaml.cs
+++ StudentBinding/MainPage.xaml.cs
@@ -1,12 +1,57 @@
-﻿namespace StudentBinding
+﻿using StudentBinding.Models;
+
+namespace StudentBinding
 {
     public partial class MainPage : ContentPage
     {
-        
+        public Student Student1 { get; set; }
+        public Student Student2 { get; set; }
+        private Student currentStudent;
+        public Student CurrentStudent
+        {
+            get => currentStudent;
+            set
+            {
+                currentStudent = value;
+                OnPropertyChanged(nameof(CurrentStudent));
+            }
+        }
+
         public MainPage()
         {
             InitializeComponent();
+            Student1 = new Student
+            {
+                Name = "מוחמד אבו נאסר",
+                Age = 99,
+                BirthDate = new DateTime(1927, 3, 18),
+                Image = "daniel.png",
+                Email = "abunasser@gmail.com",
+                Phone = "054-6767679"
+            };
+            Student2 = new Student
+            {
+                Name = "נוח מאיר",
+                Age = 52,
+                BirthDate = new DateTime(1974, 7, 19),
+                Image = "noa.png",
+                Email = "noemaier@gmail.com",
+                Phone = "054-9998567"
+            };
+            CurrentStudent = Student1;
+            this.BindingContext = this;
+        }
+        private void OnChangeStudentClicked(object sender, EventArgs e)
+        {
+            if (CurrentStudent == Student1)
+                CurrentStudent = Student2;
+            else
+                CurrentStudent = Student1;
         }
 
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
     }
 }
